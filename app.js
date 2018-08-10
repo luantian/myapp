18b5f195772d115bca18f27dd59fd6af05ba9c87
terence <133334964@qq.com>
2018-08-10 17:59:03 +0800

    添加空格2

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
+app.use(cookieParser()); 
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.use('/', router);
+app.use('/', router); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	next(createError(404));
-});
+}); 
 
 // error handler
 app.use(function(err, req, res, next) {
