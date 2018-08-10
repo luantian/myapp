5e56d4e60110700ac544f0934fc12ab5aee9cd2c
terence <133334964@qq.com>
2018-08-10 17:54:29 +0800

    aaa

--- utils/inspect_params.js
+++ utils/inspect_params.js
@@ -2,7 +2,7 @@
  * @Author: Terence 
  * @Date: 2018-06-01 00:17:09 
  * @Last Modified by: Terence
- * @Last Modified time: 2018-06-01 12:43:50
+ * @Last Modified time: 2018-08-01 17:54:49
  */
 
 const path = require('path');
@@ -36,31 +36,42 @@ const inspect_params = (req, res, next) => {
 			 * @var {String} type api_legend文件 type 字段的值
 			 * @var {Array} len  api_legend文件 len 字段的值
 			 */
+			
 			let val = req.body[key],
 				need = params[key]['need'],
 				type = params[key]['type'],
 				len = params[key]['len'];
 			if (need !== undefined && val === undefined) {
-				ret.no = 400;
-				ret.msg = `参数${key}不存在`;
+				ret = {
+					no: 400,
+					msg:  `参数${key}不存在`
+				};
 				isPass = false;
 				break;
 			}
+			
 			if (type !== undefined && !isSameType(val, type)) {
-				ret.no = 400;
-				ret.msg = `参数${key}必须为${type}类型`;
+				ret = {
+					no: 400,
+					msg:  `参数${key}必须为${type}类型`
+				};
 				isPass = false;
 				break;
 			}
 			if (len !== undefined && len[0] !== len[1] && (len[0] >= len[1] || val.length <= len[0] || val.length >= len[1])) {
-				ret.no = 400;
-				ret.msg = `参数${key}长度必须在${len[0]} ~ ${len[1]}之间`;
+				ret = {
+					no: 400,
+					msg:  `参数${key}长度必须在${len[0]} ~ ${len[1]}之间`
+				};
 				isPass = false;
 				break;
 			}
+			
 			if (len !== undefined && len[0] === len[1] && val.length != len[0]) {
-				ret.no = 400;
-				ret.msg = `参数${key}长度必须为${len[0]}`;
+				ret = {
+					no: 400,
+					msg: `参数${key}长度必须为${len[0]}`
+				};
 				isPass = false;
 				break;
 			}
@@ -78,6 +89,7 @@ const isSameType = (value, type) => {
 	let num = Object.prototype.toString.call(value).indexOf(type);
 	return num >= 0;
 }
+
 
 
 module.exports = inspect_params;
